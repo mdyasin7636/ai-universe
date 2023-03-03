19c63112a58723f5247408e43e2c9f1bfc1f1b8a
mdyasin111 <mdyasin7636@gmail.com>
2023-03-03 10:47:59 +0600

    card-data-dynamically-set

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -13,19 +13,19 @@ const displayCards = cards => {
         cardDiv.classList.add('col')
         cardDiv.innerHTML = `
         <div class="card h-100 p-2 pb-0">
-            <img src="${card.image}" class="card-img-top rounded" alt="...">
+            <img src="${card.image}" class="card-img-top rounded" style="height: 30vh;" alt="...">
             <div class="card-body pb-0">
-              <h5 class="card-title">${card.name}</h5>
+              <h5 class="card-title">Features</h5>
               <ol>
-                <li>AI</li>
-                <li>AI</li>
-                <li>AI</li>
+                <li>${card.features[0]}</li>
+                <li>${card.features[1]}</li>
+                <li>${card.features[2] ? card.features[2] : 'No Data Found'}</li>
               </ol>
               <hr />
               <div class="d-flex justify-content-between">
                 <div>
-                  <h5 class="card-title">Card title</h5>
-                  <p> <i class="fa-regular fa-calendar-days"></i> Date </p>
+                  <h5 class="card-title">${card.name}</h5>
+                  <p> <i class="fa-regular fa-calendar-days"></i> ${card.published_in}</p>
                 </div>
                 <div>
                   <i class="fa-solid fa-arrow-right pt-3 text-danger"></i>
