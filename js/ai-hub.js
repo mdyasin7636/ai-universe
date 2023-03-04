5e1db896d479b0070917b468806fd693c9f68156
mdyasin111 <mdyasin7636@gmail.com>
2023-03-04 18:42:57 +0600

    modal data added to js

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -6,6 +6,7 @@ const loadCards = () => {
     .then(data => {
       displayCards(data.data.tools.slice(0, 6))
         toggleSpinner(false)
+        
     })
 }
 
@@ -25,8 +26,8 @@ const showAllData = () => {
 const displayCards = cards => {
     const cardsContainer = document.getElementById('all-cards')
     cardsContainer.innerHTML = "";
-    const showAll = document.getElementById('show-all')
-    showAll.classList.remove('d-none')
+    // const showAll = document.getElementById('show-all')
+    // showAll.classList.remove('d-none')
     cards.forEach(card => {
         const cardDiv = document.createElement('div')
         cardDiv.classList.add('col')
@@ -47,7 +48,8 @@ const displayCards = cards => {
                   <p> <i class="fa-regular fa-calendar-days"></i> ${card.published_in}</p>
                 </div>
                 <div>
-                  <i class="fa-solid fa-arrow-right pt-3 text-danger" onclick="loadCardDetails()" data-bs-toggle="modal" data-bs-target="#cardDetailModal"></i>
+                  <i class="fa-solid fa-arrow-right pt-3 text-danger" onclick="loadCardDetails('${card.id}')" data-bs-toggle="modal" data-bs-target="#cardDetailModal">
+                  </i>
                 </div>
               </div>
             </div>
@@ -72,7 +74,49 @@ const loadCardDetails = (id) =>{
 }
 
 const showCardDetails = card =>{
-  console.log(card)
+  console.log(card);
+  const modalCards = document.getElementById('modalCards')
+  modalCards.innerHTML = `
+  <div class="row row-cols-1 row-cols-md-2">
+                <div class="border rounded bg-danger-subtle">
+                  <h4>ChatGPT is an AI-powered chatbot platform that uses OpenAI's GPT technology to simulate human
+                    conversation.</h4>
+                  <div class="row row-cols-3 m-3">
+                    <p class="border rounded bg-white text-success p-4">$10/month Basic</p>
+                    <p class="border rounded bg-white text-warning p-4">$50/month Pro</p>
+                    <p class="border rounded bg-white text-danger p-4">Contact us Enterprise</p>
+                  </div>
+                  <div class="row row-cols-2 g-2">
+                    <div>
+                      <h5>Features</h5>
+                      <ul>
+                        <li>Customizable responses</li>
+                        <li>Multilingual support</li>
+                        <li>Seamless integration</li>
+                      </ul>
+                    </div>
+                    <div>
+                      <h5>Features</h5>
+                      <ul>
+                        <li>Customizable responses</li>
+                        <li>Multilingual support</li>
+                        <li>Seamless integration</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="border rounded text-center">
+                  <div>
+                    <img style="width: 25rem;" src="Rectangle 15.png" alt="">
+                    <p class="position-absolute border rounded p-2 bg-danger text-white top-0 end-0">94% accuracy</p>
+                  </div>
+                  <h4>Hi, how are you doing today?</h4>
+                  <p>I'm doing well, thank you for asking. How can I assist you today?</p>
+                </div>
+              </div>
+  
+  `
+  
 }
 
 
@@ -90,7 +134,6 @@ const sortCardsByDate = cards => {
   cards.sort((a, b) => new Date(b.published_in) - new Date(a.published_in))
   displayCards(cards)
 }
-
 
 
 loadCards ()
